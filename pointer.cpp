41ba45ed6a361af741ca671f5d21ca2b9d4d56db
RUPESH0004 <rj9082356865@gmail.com>
2023-12-06 20:27:29 +0530

    learning about patterns

---
+++ pointer.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout <<"enter the no of rows ";
+    cin>>n;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    cout << "the another way of an printing "<<endl;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+     cout << "the another way of an printing "<<endl;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    cout << "the another way of an printing "<<endl;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    cout << "the another way of an printing "<<endl;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i+1;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    cout << "the another way of an printing "<<endl;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i+1;j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    cout << "the another way of an printing "<<endl;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout << "the another way of an printing "<<endl;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout << "the another way of an printing "<<endl;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i+1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
